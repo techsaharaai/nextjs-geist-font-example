68a7944376fb41523ad74e1fd048234b40db576d
techsaharaai <techsahara.ai@gmail.com>
2025-08-20 03:09:54 +0530

    Add apps/admin/.next/static/chunks/apps_admin_src_app_reports_page_tsx_ca76523c._.js 1755639594225

---
+++ apps/admin/.next/static/chunks/apps_admin_src_app_reports_page_tsx_ca76523c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/apps_admin_src_a7ed5dcd._.js",
+  "static/chunks/node_modules_a6354eb2._.js"
+],
+    source: "dynamic"
+});
